96258a8b9ff99030bd78c3a469f7b05ddde9f8d8
Blockmaster100 <109307115+Blockmaster100@users.noreply.github.com>
2025-04-30 19:08:39 +0100

    temp fix it lol

---
+++ index.js
@@ -0,0 +1,50 @@
+// Discord.js classes
+const fileSystem = require("node:fs")
+const path = require("node:path")
+const {Client, Collection, Events, GatewayIntentBits} = require("discord.js")
+
+// Get global variables
+const dotenv = require("dotenv")
+dotenv.config()
+
+// Create client
+const client = new Client({intents: GatewayIntentBits.Guilds})
+client.commands = new Collection()
+
+// I stole this from the docs lol
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fileSystem.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fileSystem.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Will run once when the client is loaded
+client.once(Events.ClientReady, readyClient => {
+    console.log(`${readyClient.user.tag} ready.`)
+    console.log(readyClient.application.id)
+})
+
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return
+
+    const command = interaction.client.commands.get(interaction.commandName)
+
+    if (command) {
+        await command.execute(interaction)
+    }
+})
+
+// Starts the bot
+client.login(process.env.token)
