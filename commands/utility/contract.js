07f464374e017b0e7ee2474cf6f1dfc9e793ee31
thies24n <thet3mplate@gmail.com>
2025-05-01 00:43:30 -0400

    Convert /contract_create to subcommand

--- commands/utility/contract.js
+++ commands/utility/contract.js
@@ -1,43 +1,56 @@
-const { ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder, blockQuote, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require("discord.js");
+const { ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder, blockQuote, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandSubcommandBuilder } = require("discord.js");
+
+async function handleCreate(interaction) {
+    const starSelect = new StringSelectMenuBuilder()
+        .setCustomId('star-select')
+        .setPlaceholder("Select one")
+        .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setLabel("💣")
+                .setDescription("Provided minimal to no feedback")
+                .setValue('stars-0'),
+            new StringSelectMenuOptionBuilder()
+                .setLabel("⭐")
+                .setDescription("Provided partial feedback, or subpar feedback with demonstrable effort")
+                .setValue('stars-1'),
+            new StringSelectMenuOptionBuilder()
+                .setLabel("⭐⭐")
+                .setDescription("Provided complete feedback")
+                .setValue('stars-2'),
+            new StringSelectMenuOptionBuilder()
+                .setLabel("⭐⭐⭐")
+                .setDescription("Provided thoughtful, thorough, insightful feedback; went the extra mile")
+                .setValue('stars-3')
+        );
+    
+    const accept = new ButtonBuilder()
+        .setCustomId('accept')
+        .setLabel('Accept')
+        .setStyle(ButtonStyle.Primary);
+    
+    const row = new ActionRowBuilder()
+        .addComponents(starSelect);
+
+    await interaction.reply({
+        content: `Sign ${interaction.user}'s contract bro`,
+        components: [row],
+    });
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("contract")
-        .setDescription("Create a new feedback contract"),
-    async execute(interaction) {
-        const starSelect = new StringSelectMenuBuilder()
-            .setCustomId('star-select')
-            .setPlaceholder("Select one")
-            .addOptions(
-                new StringSelectMenuOptionBuilder()
-                    .setLabel("💣")
-                    .setDescription("Provided minimal to no feedback")
-                    .setValue('stars-0'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel("⭐")
-                    .setDescription("Provided partial feedback, or subpar feedback with demonstrable effort")
-                    .setValue('stars-1'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel("⭐⭐")
-                    .setDescription("Provided complete feedback")
-                    .setValue('stars-2'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel("⭐⭐⭐")
-                    .setDescription("Provided thoughtful, thorough, insightful feedback; went the extra mile")
-                    .setValue('stars-3')
-            );
+        .setDescription("Feedback contract commands")
         
-        const accept = new ButtonBuilder()
-            .setCustomId('accept')
-            .setLabel('Accept')
-            .setStyle(ButtonStyle.Primary);
-        
-        const row = new ActionRowBuilder()
-            .addComponents(starSelect);
+        .addSubcommand(new SlashCommandSubcommandBuilder()
+            .setName("create")
+            .setDescription("Creates a feedback contract")
+        ),
 
-        await interaction.reply({
-            content: `Sign ${interaction.user}'s contract bro`,
-            components: [row],
-        });
+    async execute(interaction) {
+        switch (interaction.options.getSubcommand()) {
+            case ("create"):
+                handleCreate(interaction);
+        }
     },
 };
