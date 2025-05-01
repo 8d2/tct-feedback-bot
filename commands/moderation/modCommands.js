ae5b75d24f3bed0792917e9653fe03e3b35d7dcd
frosster <133030493+frosster@users.noreply.github.com>
2025-05-01 10:18:34 -0500

    Add mod permission check and embed styling

--- commands/moderation/modCommands.js
+++ commands/moderation/modCommands.js
@@ -1,44 +1,58 @@
-const { SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandUserOption } = require("discord.js");
-const USER_OPTION = "user"
+const { SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandUserOption, PermissionsBitField, EmbedBuilder, Colors, MessageFlags } = require("discord.js");
+
+const USER_OPTION_NAME = "user"
+const MOD_PERMS = [PermissionsBitField.Flags.Administrator, PermissionsBitField.Flags.BanMembers] // 8 (admin) or 4 (ban members)
+
+const BLOCK_COMMAND_NAME = "block"
+const UNBLOCK_COMMAND_NAME = "unblock"
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("mod")
         .setDescription("Moderator commands")
-
+        
         .addSubcommand(new SlashCommandSubcommandBuilder()
-            .setName("block")
+            .setName(BLOCK_COMMAND_NAME)
             .setDescription("Blocks a user from creating feedback contracts")
             .addUserOption(new SlashCommandUserOption()
-                .setName(USER_OPTION)
-                .setDescription("The user to block")
-                .setRequired(true)
-            )
+			.setName(USER_OPTION_NAME)
+			.setDescription("The user to block")
+			.setRequired(true)
+			)
         )
 
-        .addSubcommand(new SlashCommandSubcommandBuilder()
-            .setName("unblock")
-            .setDescription("Unblocks a user, allowing them to create feedback contracts.")
-            .addUserOption(new SlashCommandUserOption()
-                .setName(USER_OPTION)
-                .setDescription("The user to unblock")
-                .setRequired(true)
-            )
-        ),
+		.addSubcommand(new SlashCommandSubcommandBuilder()
+			.setName(UNBLOCK_COMMAND_NAME)
+			.setDescription("Unblocks a user, allowing them to create feedback contracts.")
+			.addUserOption(new SlashCommandUserOption()
+			.setName(USER_OPTION_NAME)
+			.setDescription("The user to unblock")
+			.setRequired(true)
+			)
+		),
 
     async execute(interaction) {
-        // check that the user has moderator permissions
-        //perms = interactions.member.permissions
-        //if (perms.has(
-
-        subcommandName = interaction.options.getSubcommand()
-        if (subcommandName === "block") {
-            blockeeName = interaction.options.getUser(USER_OPTION).displayName
-            await interaction.reply("**" + blockeeName + "** has been blocked from creating feedback contracts.")
-        }
-        else if (subcommandName === "unblock") {
-            unblockeeName = interaction.options.getUser(USER_OPTION).displayName
-            await interaction.reply("**" + unblockeeName + "** has been unblocked and can now create feedback contracts.")
-        }
+		// check that the user has moderator permissions
+		if (!interaction.member.permissions.any(MOD_PERMS)) return
+		
+		let newEmbed = new EmbedBuilder().setTimestamp()
+		
+		// action based on subcommand
+		switch (interaction.options.getSubcommand()) {
+			case (BLOCK_COMMAND_NAME):
+				let blockee = interaction.options.getUser(USER_OPTION_NAME)
+				newEmbed.setDescription(`${blockee} has been blocked from creating feedback contracts.`)
+				newEmbed.setColor(Colors.Red)
+				break
+			case (UNBLOCK_COMMAND_NAME):
+				let unblockee = interaction.options.getUser(USER_OPTION_NAME)
+				newEmbed.setDescription(`${unblockee} has been unblocked and can now create feedback contracts.`)
+				newEmbed.setColor(Colors.Green)
+				break
+			default:
+				break
+		}
+		
+		await interaction.reply({embeds: [newEmbed]})
     },
 }
