85ca8c7c0267b5b7084ed45936e84c12fc7fb832
frosster <133030493+frosster@users.noreply.github.com>
2025-05-01 10:24:27 -0500

    Fix indentation

--- commands/moderation/modCommands.js
+++ commands/moderation/modCommands.js
@@ -15,44 +15,44 @@ module.exports = {
             .setName(BLOCK_COMMAND_NAME)
             .setDescription("Blocks a user from creating feedback contracts")
             .addUserOption(new SlashCommandUserOption()
-			.setName(USER_OPTION_NAME)
-			.setDescription("The user to block")
-			.setRequired(true)
-			)
+                .setName(USER_OPTION_NAME)
+                .setDescription("The user to block")
+                .setRequired(true)
+            )
         )
 
-		.addSubcommand(new SlashCommandSubcommandBuilder()
-			.setName(UNBLOCK_COMMAND_NAME)
-			.setDescription("Unblocks a user, allowing them to create feedback contracts.")
-			.addUserOption(new SlashCommandUserOption()
-			.setName(USER_OPTION_NAME)
-			.setDescription("The user to unblock")
-			.setRequired(true)
-			)
-		),
+        .addSubcommand(new SlashCommandSubcommandBuilder()
+            .setName(UNBLOCK_COMMAND_NAME)
+            .setDescription("Unblocks a user, allowing them to create feedback contracts.")
+            .addUserOption(new SlashCommandUserOption()
+                .setName(USER_OPTION_NAME)
+                .setDescription("The user to unblock")
+                .setRequired(true)
+            )
+        ),
 
     async execute(interaction) {
-		// check that the user has moderator permissions
-		if (!interaction.member.permissions.any(MOD_PERMS)) return
-		
-		let newEmbed = new EmbedBuilder().setTimestamp()
-		
-		// action based on subcommand
-		switch (interaction.options.getSubcommand()) {
-			case (BLOCK_COMMAND_NAME):
-				let blockee = interaction.options.getUser(USER_OPTION_NAME)
-				newEmbed.setDescription(`${blockee} has been blocked from creating feedback contracts.`)
-				newEmbed.setColor(Colors.Red)
-				break
-			case (UNBLOCK_COMMAND_NAME):
-				let unblockee = interaction.options.getUser(USER_OPTION_NAME)
-				newEmbed.setDescription(`${unblockee} has been unblocked and can now create feedback contracts.`)
-				newEmbed.setColor(Colors.Green)
-				break
-			default:
-				break
-		}
-		
-		await interaction.reply({embeds: [newEmbed]})
+        // check that the user has moderator permissions
+        if (!interaction.member.permissions.any(MOD_PERMS)) return
+
+        let newEmbed = new EmbedBuilder().setTimestamp()
+
+        // action based on subcommand
+        switch (interaction.options.getSubcommand()) {
+            case (BLOCK_COMMAND_NAME):
+                let blockee = interaction.options.getUser(USER_OPTION_NAME)
+                newEmbed.setDescription(`${blockee} has been blocked from creating feedback contracts.`)
+                newEmbed.setColor(Colors.Red)
+                break
+            case (UNBLOCK_COMMAND_NAME):
+                let unblockee = interaction.options.getUser(USER_OPTION_NAME)
+                newEmbed.setDescription(`${unblockee} has been unblocked and can now create feedback contracts.`)
+                newEmbed.setColor(Colors.Green)
+                break
+            default:
+                break
+        }
+
+        await interaction.reply({embeds: [newEmbed]})
     },
 }
