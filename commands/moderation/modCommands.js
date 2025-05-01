3a0157ce5ff8bafaa6d423f03d33f1063f8cc877
frosster <133030493+frosster@users.noreply.github.com>
2025-05-01 14:54:16 -0500

    Add missing semicolons

--- commands/moderation/modCommands.js
+++ commands/moderation/modCommands.js
@@ -1,23 +1,23 @@
 const { SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandUserOption, PermissionsBitField, EmbedBuilder, Colors, MessageFlags } = require("discord.js");
 
-const USER_OPTION_NAME = "user"
-const MOD_PERMS = [PermissionsBitField.Flags.Administrator, PermissionsBitField.Flags.BanMembers] // 8 (admin) or 4 (ban members)
+const USER_OPTION_NAME = "user";
+const MOD_PERMS = [PermissionsBitField.Flags.Administrator, PermissionsBitField.Flags.BanMembers]; // 8 (admin) or 4 (ban members)
 
-const BLOCK_COMMAND_NAME = "block"
-const UNBLOCK_COMMAND_NAME = "unblock"
+const BLOCK_COMMAND_NAME = "block";
+const UNBLOCK_COMMAND_NAME = "unblock";
 
 // Handles the '/mod block' command.
 // interaction: the interaction that used this command
 // messageEmbed: the embed to modify and reply with
 // returns false if the action failed.
 async function handleBlock(interaction, messageEmbed) {
-    let blockee = interaction.options.getUser(USER_OPTION_NAME)
+    let blockee = interaction.options.getUser(USER_OPTION_NAME);
     
     // TODO: check if the blockee is already blocked, if so then notify the command user.
     
-    messageEmbed.setDescription(`${blockee} has been blocked from creating feedback contracts.`)
-    messageEmbed.setColor(Colors.Red)
-    return true
+    messageEmbed.setDescription(`${blockee} has been blocked from creating feedback contracts.`);
+    messageEmbed.setColor(Colors.Red);
+    return true;
 }
 
 // Handles the '/mod unblock' command.
@@ -25,13 +25,13 @@ async function handleBlock(interaction, messageEmbed) {
 // messageEmbed: the embed to modify and reply with
 // returns false if the action failed.
 async function handleUnblock(interaction, messageEmbed) {
-    let unblockee = interaction.options.getUser(USER_OPTION_NAME)
+    let unblockee = interaction.options.getUser(USER_OPTION_NAME);
     
     // TODO: check if the unblockee is not blocked, if so then notify the command user.
     
-    messageEmbed.setDescription(`${unblockee} has been unblocked and can now create feedback contracts.`)
-    messageEmbed.setColor(Colors.Green)
-    return true
+    messageEmbed.setDescription(`${unblockee} has been unblocked and can now create feedback contracts.`);
+    messageEmbed.setColor(Colors.Green);
+    return true;
 }
 
 module.exports = {
@@ -61,28 +61,28 @@ module.exports = {
 
     async execute(interaction) {
         // check that the user has moderator permissions
-        if (!interaction.member.permissions.any(MOD_PERMS)) return
+        if (!interaction.member.permissions.any(MOD_PERMS)) return;
 
-        let newEmbed = new EmbedBuilder().setTimestamp()
-        let successful = false
+        let newEmbed = new EmbedBuilder().setTimestamp();
+        let successful = false;
 
         // action based on subcommand
         switch (interaction.options.getSubcommand()) {
             case (BLOCK_COMMAND_NAME):
-                successful = handleBlock(interaction, newEmbed)
-                break
+                successful = handleBlock(interaction, newEmbed);
+                break;
             case (UNBLOCK_COMMAND_NAME):
-                successful = handleUnblock(interaction, newEmbed)
-                break
+                successful = handleUnblock(interaction, newEmbed);
+                break;
             default:
-                break
+                break;
         }
         
         if (successful) {
-            await interaction.reply({embeds: [newEmbed]})
+            await interaction.reply({embeds: [newEmbed]});
         }
         else {
-            await interaction.reply({embeds: [newEmbed], flags: MessageFlags.Ephemeral})
+            await interaction.reply({embeds: [newEmbed], flags: MessageFlags.Ephemeral});
         }
     },
-}
+};
