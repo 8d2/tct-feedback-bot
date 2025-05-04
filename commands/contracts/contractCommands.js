d9ee0ee360b8e6485b1b7c4434413bde24fc5206
thies24n <thet3mplate@gmail.com>
2025-05-03 21:16:40 -0400

    Move handleContractCreate back to contractCommands.js

--- commands/contracts/contractCommands.js
+++ commands/contracts/contractCommands.js
@@ -1,5 +1,16 @@
 const { SlashCommandBuilder, SlashCommandSubcommandBuilder } = require("discord.js");
-const { handleContractCreate } = require("./contract_create");
+const { createContractMessage } = require("../../handlers/contract");
+
+/**
+ * Handles the `/contract create` subcommand.
+ * @param {CommandInteractionOptionResolver} interaction The interaction that used this command.
+ */
+async function handleContractCreate(interaction) {
+
+    // Component creation has been outsourced to handlers </3
+    await interaction.reply(createContractMessage(interaction));
+
+};
 
 module.exports = {
     data: new SlashCommandBuilder()
