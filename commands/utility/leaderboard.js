1594b25f8656097174e7c78a7e558cf25ecbd9f9
Blockmaster100 <109307115+Blockmaster100@users.noreply.github.com>
2025-05-08 19:12:33 +0100

    Get and order user points and display them in a message

--- commands/utility/leaderboard.js
+++ commands/utility/leaderboard.js
@@ -8,11 +8,36 @@ module.exports = {
         .setDescription("View the top 10 feedbackers with the most points"),
 
     async execute(interaction) {
+        // Get a list of user ids which have data
+        const listOfUserIds = await userMethods.getIdsWithInfo()
+
+        // Loop through all ids and get their points
+        var rawLeaderboardStats = {}
+        for (var i = 0; i < listOfUserIds.length; i++) {
+            const id = listOfUserIds[i]
+
+            // Discard users with 0 points
+            const points = await userMethods.getPoints(id)
+            if (points > 0) {
+                rawLeaderboardStats[id] = points
+            }
+        }
+        
+        // Order the dictionary by most to least points
+        const sortedLeaderboardStats = Object.fromEntries(Object.entries(rawLeaderboardStats).sort((a, b) => b[1] - a[1]));
+
+        // Create the contents of the leaderboard message
+        var appendedLeaderboardMessage = ""
+        var cycle = 0
+        for (let userId in sortedLeaderboardStats) {
+            cycle += 1
+            appendedLeaderboardMessage += `${cycle}. <@${userId}> - ${sortedLeaderboardStats[userId]} points\n`
+        }
 
         const responseEmbed = new EmbedBuilder()
             .setTimestamp()
             .setColor(Colors.Purple)
-            .setDescription("## Leaderboard\n")
+            .setDescription("## Leaderboard\n" + appendedLeaderboardMessage)
         await interaction.reply({embeds: [responseEmbed]});
     },
 }
