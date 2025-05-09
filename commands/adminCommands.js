f5fb753849eb12d52e0c212fc275451ec168e22b
ben-turtles <87501306+benvx@users.noreply.github.com>
2025-05-09 00:25:03 -0400

    Update adminCommands.js

--- commands/adminCommands.js
+++ commands/adminCommands.js
@@ -73,7 +73,7 @@ const COMMAND_FUNCTIONS = {
         messageEmbed.setColor(Colors.Green);
         if (updateAllRoles) {
             const responseEmbed = await userMethods.updateAllUsersRoles(interaction);
-            return {doFollowUpPing: true, followUpEmbeds: [responseEmbed]};
+            return {followUpEmbeds: [responseEmbed]};
         }
         return true;
     },
@@ -120,7 +120,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("admin")
         .setDescription("administrator commands")
-        // .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         
         .addSubcommand(new SlashCommandSubcommandBuilder()
             .setName(SET_CHANNEL_COMMAND_NAME)
