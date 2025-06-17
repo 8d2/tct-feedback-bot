15c5172b12f0b5b0bc59c46c5ea3f3ae6b1b9c1e
8d2 <thet3mplate@gmail.com>
2025-06-17 01:09:45 -0400

    Add is_blocked and is_collaborator

--- models/ThreadUsers.js
+++ models/ThreadUsers.js
@@ -13,6 +13,24 @@ module.exports = (sequelize, DataTypes) => {
 			type: DataTypes.BIGINT.UNSIGNED,
             allowNull: false
 		},
+        is_blocked: {
+            // Whether the user (corresponding to user_id) is blocked from 
+            // posting contracts in the thread (corresponding to thread_id).
+
+            // This is not implemented and is not specified in any design documents.
+            // However, this feature may become a necessity in the future,
+            // and updating the database post-deployment would kinda suck.
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        },
+        is_collaborator: {
+            // Whether the user (corresponding to user_id) is a collaborator
+            // in the thread (corresponding to thread_id).
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        },
         last_contract_posted: {
             // A date representing when the user last posted a contract
             // in this thread.
